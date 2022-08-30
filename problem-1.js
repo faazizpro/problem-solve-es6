12f20e79d247b6182ca4a1189aaa6aba5b248806
faazizpro <fahadafs11@gmail.com>
2022-08-30 23:33:52 +0600

    Practice Problem Module 30 Starts

---
+++ problem-1.js
@@ -0,0 +1,18 @@
+// 1) Write an arrow function that will take 3 parameters, will multiply
+// the parameters and will return the result.
+
+const multiply = (a, b, c) => a * b * c;
+const numbers= multiply(2, 4, 6);
+console.log(numbers);
+
+/*
+2) Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani.
+*/
+
+const threeLines = `
+I am a web developer and amazon affiliate marketer.
+I love to code.
+I love to eat kacchi.`;
+console.log(threeLines);
+
