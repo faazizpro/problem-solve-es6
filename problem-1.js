a700de2ffb474c5af12b851aeccb0132da4d1f05
faazizpro <fahadafs11@gmail.com>
2022-08-30 23:35:15 +0600

    Practice Problem 1 Done

--- problem-1.js
+++ problem-1.js
@@ -16,3 +16,12 @@ I love to code.
 I love to eat kacchi.`;
 console.log(threeLines);
 
+/*
+3) Write an arrow function that will take 2 parameters: One parameter
+will come from you and the other parameter will be a default
+parameter. Add these two parameters and return the result.
+*/
+
+const arrayNum = (first, second = 5) => first / second;
+const enter = arrayNum(50);
+console.log(enter);
